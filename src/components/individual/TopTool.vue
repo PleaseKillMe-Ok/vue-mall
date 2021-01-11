7a7f522ad916733e70aba7b4cf8d7c83dd3ff3c2
syz <syz247179876@126.com>
2021-01-11 20:59:49 +0800

    1.用router-view代替App中固定的组件.2.添加搜索组件,头像组件,个人空间组件等

---
+++ src/components/individual/TopTool.vue
@@ -0,0 +1,27 @@
+<template>
+  <div name="TopTool">
+    <van-nav-bar
+      title="个人中心"
+      right-text="个人信息"
+      @click-right="disPlayInformation"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopTool",
+  data() {
+    return {};
+  },
+  methods: {
+    // 跳转到个人详细信息页
+    disPlayInformation() {
+        this.$router.push("/individual/information")
+    },
+  },
+};
+</script>
+
+<style>
+</style>
