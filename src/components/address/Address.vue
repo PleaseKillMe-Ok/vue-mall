ada41d1bf3e4228971bb3ef70f46e185a2e19352
syz247179876 <247179876@qq.com>
2021-01-27 22:26:14 +0800

    1.修改文件命名格式  2.增加账户设置组件和收货地址组件

---
+++ src/components/address/Address.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="Address">
+    <van-address-list
+      v-for="item in addressList"
+      :key="item.id"
+      default-tag-text="默认"
+      v-model="chosenAddressId"
+      @add="onAdd"
+      @edit="onEdit"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Address",
+  data() {
+    return {
+      chosenAddressId: "1",  // 选中的列表id
+      addressList: [
+        {
+          id: "1",
+          name: "司云中",
+          tel: "13787833290",
+          address: "江苏省扬州市纤城小区9栋106",
+          isDefault: true,
+        },
+        {
+          id: "2",
+          name: "李四",
+          tel: "1310000000",
+          address: "江苏省扬州市纤城小区9栋106",
+        },
+        {
+          id: "3",
+          name: "李四",
+          tel: "1310000000",
+          address: "江苏省扬州市纤城小区9栋106",
+        },
+      ],
+    };
+  },
+  methods:{
+      // 新增地址
+      onAdd(){},
+      // 编辑
+      onEdit(item, index){}
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/user/UserSettings.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- 用户设置 -->
+  <div id="UserSettings">
+    <TopTool :title="账户设置" :hasRight="false"></TopTool>
+    <van-cell-group>
+      <van-cell is-link>
+        <template #title>
+          <span class="title"
+            ><van-image
+              round
+              width="5rem"
+              height="5rem"
+              src="https://img01.yzcdn.cn/vant/cat.jpeg"
+          /></span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">地址管理</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">我的账户管理</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">支付设置</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">地区设置</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">问题反馈</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">关于吃货商城</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">切换用户</span>
+        </template>
+      </van-cell>
+      <van-cell is-link>
+        <template #title>
+          <span class="title">退出登录</span>
+        </template>
+      </van-cell>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+const TopTool = () => import("@/components/user/TopTool");
+export default {
+  name: "UserSettings",
+  components: { TopTool },
+};
+</script>
+
+<style scoped>
+.title {
+  float: left;
+  font-size: 0.9rem;
+}
+</style>
