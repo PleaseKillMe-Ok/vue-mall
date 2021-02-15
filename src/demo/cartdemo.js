9eb5b5558ad7713358f82146a2654ed0d02eac45
syz247179876 <247179876@qq.com>
2021-02-15 22:16:06 +0800

    继续设计购物车布局

---
+++ src/demo/cartdemo.js
@@ -0,0 +1,31 @@
+var storeList = {
+    '艾诺迪亚数码专营': [{
+        'intro': '15.6英寸便携显示器铝合金桌面',
+        'price': 59.4,
+        'favourable_price': 50.9,
+        'count': 2
+    }],
+    '天地图书专营店': [{
+        'intro': '全部现货[重点题视频]2021',
+        'price': 41,
+        'favourable_price': 39.5,
+        'count': 3
+    }, {
+        'intro': '全部现货[送攻略+易错题]2021',
+        'price': 149,
+        'favourable_price': 125,
+        'count': 3
+    }, {
+        'intro': '全部现货2022张键',
+        'price': 199.9,
+        'favourable_price': 189,
+        'count': 1
+    }]
+}
+
+var storeNameDict = {
+    '艾诺迪亚数码专营': false,
+    '天地图书专营店': false
+}
+
+export { storeList, storeNameDict }
