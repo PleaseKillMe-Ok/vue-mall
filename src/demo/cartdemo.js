c887ec51251e54291a22b0b3209b1d93936d4249
syz247179876 <247179876@qq.com>
2021-04-11 22:17:57 +0800

    初步完成购物车界面布局

--- src/demo/cartdemo.js
+++ src/demo/cartdemo.js
@@ -1,25 +1,38 @@
 var storeList = {
     '艾诺迪亚数码专营': [{
+        'id': 1,
         'intro': '15.6英寸便携显示器铝合金桌面',
         'price': 59.4,
         'favourable_price': 50.9,
-        'count': 2
+        'count': 2,
+        'openCount': false,
+        'little_image': "https://django-e-mall.oss-cn-shanghai.aliyuncs.com/u%3D3324287611%2C3832720410%26fm%3D26%26gp%3D0.jpg"
+
     }],
     '天地图书专营店': [{
+        'id': 2,
         'intro': '全部现货[重点题视频]2021',
         'price': 41,
         'favourable_price': 39.5,
-        'count': 3
+        'count': 3,
+        'openCount': false,
+        'little_image': "https://django-e-mall.oss-cn-shanghai.aliyuncs.com/u%3D3324287611%2C3832720410%26fm%3D26%26gp%3D0.jpg"
     }, {
+        'id': 3,
         'intro': '全部现货[送攻略+易错题]2021',
         'price': 149,
         'favourable_price': 125,
-        'count': 3
+        'count': 3,
+        'openCount': false,
+        'little_image': "https://django-e-mall.oss-cn-shanghai.aliyuncs.com/u%3D3324287611%2C3832720410%26fm%3D26%26gp%3D0.jpg"
     }, {
+        'id': 4,
         'intro': '全部现货2022张键',
         'price': 199.9,
         'favourable_price': 189,
-        'count': 1
+        'count': 1,
+        'openCount': false,
+        'little_image': "https://django-e-mall.oss-cn-shanghai.aliyuncs.com/u%3D3324287611%2C3832720410%26fm%3D26%26gp%3D0.jpg"
     }]
 }
 
