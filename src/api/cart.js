b527a1dbb80c25ed2696a41fb5c6d23ceb7faa32
syz247179876 <247179876@qq.com>
2021-04-23 11:27:19 +0800

    添加购物车相关API

--- src/api/cart.js
+++ src/api/cart.js
@@ -3,10 +3,35 @@ import axios from '@/axios'
 /**
  * 将目标商品添加进购物车
  */
-export function addToCart(id) {
-    return axios.get('')
+export function addToCart(data) {
+    return axios.get('/consumer/chsc/apis/trolley/', data)
 }
 
 /**
- * 
+ * 修改购物车中某商品的数量
  */
+export function modifySkuCount(data) {
+    return axios.put('/consumer/chsc/apis/trolley/count/', data)
+}
+
+/**
+ * 获取购物车中商品信息
+ */
+export function getCartInfo() {
+    return axios.get('/consumer/chsc/apis/trolley/')
+}
+
+
+/**
+ * 删除购物车中所有数据
+ */
+export function deleteCartAll() {
+    return axios.delete('/consumer/chsc/apis/trolley/all/')
+}
+
+/**
+ * 删除指定购物车中的记录
+ */
+export function deleteCartSeveral(data) {
+    return axios.delete('/consumer/chsc/apis/trolley/several', data)
+}
